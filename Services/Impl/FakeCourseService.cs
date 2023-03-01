9773a22d7e257c246a04a6ef795a8b2cee5fbd8c
Phi Van Ngoc <ngocketit@gmail.com>
2023-03-01 22:00:04 +0200

    Fix async warnings

--- Services/Impl/FakeCourseService.cs
+++ Services/Impl/FakeCourseService.cs
@@ -5,11 +5,13 @@ namespace NETCoreDemo.Services;
 
 public class FakeCourseSerivce : FakeCrudService<Course, CourseDTO>, ICourseService
 {
-    // TODO: Fix the warning
     public async Task<ICollection<Course>> GetCoursesByStatusAsync(Course.CourseStatus status)
     {
-        return _items.Values
-            .Where(c => c.Status == status)
-            .ToList();
+        return await Task.Run(() =>
+        {
+            return _items.Values
+                .Where(c => c.Status == status)
+                .ToList();
+        });
     }
 }
--- Services/Impl/FakeCrudService.cs
+++ Services/Impl/FakeCrudService.cs
@@ -4,7 +4,6 @@ using NETCoreDemo.Models;
 
 namespace NETCoreDemo.Services;
 
-// TODO: Fix the async warnings in the methods
 public class FakeCrudService<TModel, TDto> : ICrudService<TModel, TDto>
     where TModel : BaseModel, new()
     where TDto : BaseDTO<TModel>
@@ -14,46 +13,58 @@ public class FakeCrudService<TModel, TDto> : ICrudService<TModel, TDto>
 
     public async Task<TModel?> CreateAsync(TDto request)
     {
-        var item = new TModel
+        return await Task.Run(() =>
         {
-            Id = Interlocked.Increment(ref _itemId), // Atomic operation
-        };
-        _items[item.Id] = item;
-        request.UpdateModel(item);
-        return item;
+            var item = new TModel
+            {
+                Id = Interlocked.Increment(ref _itemId), // Atomic operation
+            };
+            _items[item.Id] = item;
+            request.UpdateModel(item);
+            return item;
+        });
     }
 
     public async Task<bool> DeleteAsync(int id)
     {
-        if (!_items.ContainsKey(id))
+        return await Task.Run(() =>
         {
-            return false;
-        }
-        return _items.Remove(id, out var _);
+            if (!_items.ContainsKey(id))
+            {
+                return false;
+            }
+            return _items.Remove(id, out var _);
+        });
     }
 
     public async Task<TModel?> GetAsync(int id)
     {
-        if (_items.TryGetValue(id, out var item))
+        return await Task.Run(() =>
         {
-            return item;
-        }
-        return null;
+            if (_items.TryGetValue(id, out var item))
+            {
+                return item;
+            }
+            return null;
+        });
     }
 
-    public async Task<ICollection<TModel>> GetAllAsync()
+    public Task<ICollection<TModel>> GetAllAsync()
     {
-        return _items.Values;
+        return Task.FromResult(_items.Values);
     }
 
     public async Task<TModel?> UpdateAsync(int id, TDto request)
     {
-        var item = await GetAsync(id);
-        if (item is null)
+        return await Task.Run(async () =>
         {
-            return null;
-        }
-        request.UpdateModel(item);
-        return item;
+            var item = await GetAsync(id);
+            if (item is null)
+            {
+                return null;
+            }
+            request.UpdateModel(item);
+            return item;
+        });
     }
 }
