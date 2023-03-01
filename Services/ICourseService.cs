5fec71520a179bdb1b18b7863e539540795f23af
Phi Van Ngoc <ngocketit@gmail.com>
2023-03-01 22:12:24 +0200

    Add TODO items

--- Services/ICourseService.cs
+++ Services/ICourseService.cs
@@ -6,4 +6,5 @@ using NETCoreDemo.DTOs;
 public interface ICourseService : ICrudService<Course, CourseDTO>
 {
     Task<ICollection<Course>> GetCoursesByStatusAsync(Course.CourseStatus status);
+    // TODO: Add and implement a method to remove a student from a course
 }
--- Services/IStudentService.cs
+++ Services/IStudentService.cs
@@ -7,4 +7,6 @@ public interface IStudentService : ICrudService<Student, StudentDTO>
 {
     ICollection<Student> GetTopStudents();
     ICollection<Student> GetStudentsWithJobs();
+    // TODO: Add and implement a method to load students by course
+    // TODO: Add and implement a method to list students from a city
 }
